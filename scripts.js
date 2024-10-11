33372f9e622a3b56f64246119a81c9b0beb8c2a6
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-11 18:02:37 +0200

    add-burgerImg-and-add-scripts-to-toggle-the-nav

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const closeImg = document.querySelector('.closeImg');
     const nav = document.querySelector('.fixed-nav');
+    const burger = document.querySelector('.burgerImg');
 
     closeImg.addEventListener('click', function() {
         nav.classList.toggle('hidden');
+        burger.classList.toggle('hidden');
+    });
+
+    const burgerImg = document.querySelector('.burgerImg');
+    burgerImg.addEventListener('click', function() {
+        nav.classList.toggle('hidden');
+        burger.classList.toggle('hidden');
     });
 });
