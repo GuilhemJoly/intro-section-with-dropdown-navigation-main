60a7b4a3ff8cf99d3659b4e1052d6b2596d5e891
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-14 12:02:45 +0200

    Update_nav_dropdown_menu: added scripts to close menus when a click in the window is applied

--- dropdownScripts.js
+++ dropdownScripts.js
@@ -1,15 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
     const closeFeatures = document.querySelector('.featuresDropdownMenu');
-    const featuresNavDropdown = document.querySelector('.featuresNavDropdown ');
+    const featuresNavDropdown = document.querySelector('.featuresNavDropdown');
 
-    closeFeatures.addEventListener('click', function() {
+    closeFeatures.addEventListener('click', function(event) {
+        event.stopPropagation();
         featuresNavDropdown.classList.toggle('hiddenDropdown');
     });
-    
+
     const closeCompany = document.querySelector('.companyDropdownMenu');
     const companyNavDropdown = document.querySelector('.companyNavDropdown');
 
-    closeCompany.addEventListener('click', function() {
+    closeCompany.addEventListener('click', function(event) {
+        event.stopPropagation();
         companyNavDropdown.classList.toggle('hiddenDropdown');
+    });
+
+    document.addEventListener('click', function() {
+        if (!featuresNavDropdown.classList.contains('hiddenDropdown')) {
+            featuresNavDropdown.classList.add('hiddenDropdown');
+        }
+        if (!companyNavDropdown.classList.contains('hiddenDropdown')) {
+            companyNavDropdown.classList.add('hiddenDropdown');
+        }
+    });
+
+    featuresNavDropdown.addEventListener('click', function(event) {
+        event.stopPropagation();
+    });
+
+    companyNavDropdown.addEventListener('click', function(event) {
+        event.stopPropagation();
     });
 });
