46576d6e4385fe53eae90d7f7da278feb8f2c78a
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-15 22:33:02 +0200

    add_grey_background_in_mobile_nav: adding a div having the navGreyBackground class which receives the greyBackground class when the navigation is open

--- scripts/openBurgerNavScripts.js
+++ scripts/openBurgerNavScripts.js
@@ -2,15 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
     const closeImg = document.querySelector('.closeImg');
     const nav = document.querySelector('.fixed-nav');
     const burger = document.querySelector('.burgerImg');
+    const body = document.querySelector('.navGreyBackground');
 
     closeImg.addEventListener('click', function() {
         nav.classList.toggle('hidden');
         burger.classList.toggle('hidden');
+        body.classList.toggle('greyBackground');
     });
 
     const burgerImg = document.querySelector('.burgerImg');
     burgerImg.addEventListener('click', function() {
         nav.classList.toggle('hidden');
         burger.classList.toggle('hidden');
+        body.classList.toggle('greyBackground')
     });
 });
