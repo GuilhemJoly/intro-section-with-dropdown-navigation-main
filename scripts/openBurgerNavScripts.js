25ca2a2b94fd556251bf7772f87fc9402c4f9081
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-15 22:38:47 +0200

    add_no_scroll_when_mobile_nav_is_open: adding noScroll css class which is applied in the body when navigation is open

--- scripts/openBurgerNavScripts.js
+++ scripts/openBurgerNavScripts.js
@@ -2,18 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const closeImg = document.querySelector('.closeImg');
     const nav = document.querySelector('.fixed-nav');
     const burger = document.querySelector('.burgerImg');
-    const body = document.querySelector('.navGreyBackground');
+    const greyBackground = document.querySelector('.navGreyBackground');
+    const body = document.querySelector('body');
 
     closeImg.addEventListener('click', function() {
         nav.classList.toggle('hidden');
         burger.classList.toggle('hidden');
-        body.classList.toggle('greyBackground');
+        greyBackground.classList.toggle('greyBackground');
+        body.classList.remove('noScroll');
     });
-
+    
     const burgerImg = document.querySelector('.burgerImg');
     burgerImg.addEventListener('click', function() {
         nav.classList.toggle('hidden');
         burger.classList.toggle('hidden');
-        body.classList.toggle('greyBackground')
+        greyBackground.classList.toggle('greyBackground')
+        body.classList.add('noScroll');
     });
 });
