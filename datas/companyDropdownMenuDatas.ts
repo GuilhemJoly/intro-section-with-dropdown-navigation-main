3e69bf39debb52c3d8df10b2eb5c5aceab3164a3
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-16 14:16:20 +0200

    adding-dynamic-datas: creating companyDropdownMenuDatas.ts with title decription and link

---
+++ datas/companyDropdownMenuDatas.ts
@@ -0,0 +1,17 @@
+export const companyDropdownMenuDatas = [
+    {
+        title: 'History',
+        description: 'Link to the History page',
+        link: '#',
+    },
+    {
+        title: 'Our Team',
+        description: 'Link to the Our Team page',
+        link: '#',
+    },
+    {
+        title: 'Blog',
+        description: 'Link to the Blog page',
+        link: '#',
+    },
+];
