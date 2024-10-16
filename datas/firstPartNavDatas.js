e2a9d07ee829f1d099d4f2e1f2a81d66fb353e95
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-16 21:28:19 +0200

    adding-dynamic-datas: create firstPartNavDatas.js

---
+++ datas/firstPartNavDatas.js
@@ -0,0 +1,26 @@
+export const firstPartNavDatas = [
+    {
+      class: 'features',
+      linkClass: 'featuresDropdownMenu',
+      linkText: 'Features',
+      imgSrc: 'images/icon-arrow-down.svg',
+      dropdownId: 'featuresDropdown',
+      dropdownClass: 'featuresNavDropdown hiddenDropdown',
+    },
+    {
+      class: 'company',
+      linkClass: 'companyDropdownMenu',
+      linkText: 'Company',
+      imgSrc: 'images/icon-arrow-down.svg',
+      dropdownId: 'companyDropdown',
+      dropdownClass: 'companyNavDropdown hiddenDropdown',
+    },
+    {
+      linkText: 'Careers',
+      linkHref: '#',
+    },
+    {
+      linkText: 'About',
+      linkHref: '#',
+    },
+  ];
