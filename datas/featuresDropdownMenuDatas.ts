242da49af0f31fd9a90e2c3f90d530cbf92a6e70
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-16 14:13:03 +0200

    adding-dynamic-datas: creating featuresDropdownMenuDatas.ts with title decription icon src and link

---
+++ datas/featuresDropdownMenuDatas.ts
@@ -0,0 +1,26 @@
+export const featuresDropdownMenuDatas = [
+    {
+        title: 'Todo List',
+        description: 'Link to the todo list page',
+        iconSrc: 'images/icon-todo.svg',
+        link: '#',
+    },
+    {
+        title: 'Calendar',
+        description: 'Link to the calendar page',
+        iconSrc: 'images/icon-calendar.svg',
+        link: '#',
+    },
+    {
+        title: 'Reminders',
+        description: 'Link to the reminders page',
+        iconSrc: 'images/icon-reminders.svg',
+        link: '#',
+    },
+    {
+        title: 'Planning',
+        description: 'Link to the planning page',
+        iconSrc: 'images/icon-planning.svg',
+        link: '#',
+    },
+    ];
